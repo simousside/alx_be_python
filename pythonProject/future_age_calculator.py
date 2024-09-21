5edbb6803a2d4b727b63876fe66a2d645e467e72
Abdelmajid <ahmedallaoui1936@gmail.com>
2024-09-21 21:55:15 +0100

    Adding the file future_age_calculator.py

--- pythonProject/future_age_calculator.py
+++ pythonProject/future_age_calculator.py
@@ -1,13 +1,5 @@
-# Prompt the user for their current age and validate input
-current_age = input("How old are you? ")
-
-# Keep asking for input until a valid integer is entered
-while not current_age.isdigit():
-    print("Please enter a valid integer.")
-    current_age = input("How old are you? ")
-
-# Convert the valid input to an integer
-current_age = int(current_age)
+# Prompt the user for their current age
+current_age = int(input("How old are you? "))
 
 # Calculate age in 2050
 age_in_2050 = current_age + 27
