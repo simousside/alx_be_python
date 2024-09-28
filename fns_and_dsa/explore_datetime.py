e9ceda24bc84ea3cd65d943190694866422f1ea4
Abdelmajid <ahemedallaoui1936@gmail.com>
2024-09-28 18:05:34 +0100

    Added a simply adjusting the input prompt to a different message in functions in explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,16 +6,17 @@ def display_current_datetime():
     current_date = datetime.now()
 
     formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+
     print(f"Current Date and Time: {formatted_date}")
 
 
 def calculate_future_date():
 
-    days_to_add = input("Enter the number of days to add to the current date:")
+    days_to_add = input("Enter the number of days to add to the current date: ")
 
     while not days_to_add.isdigit():
         print("Invalid input. Please enter a positive integer.")
-        days_to_add = input("Enter the number of days to add to the current date:")
+        days_to_add = input("Enter the number of days to add to the current date: ")
 
     days_to_add = int(days_to_add)
 
@@ -24,6 +25,7 @@ def calculate_future_date():
     future_date = current_date + timedelta(days=days_to_add)
 
     formatted_future_date = future_date.strftime("%Y-%m-%d")
+
     print(f"Future Date: {formatted_future_date}")
 
 
