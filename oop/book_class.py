6b69c0206eb472409d36aa31d5105cce6fed764a
Abdelmajid <ahemedallaoui1936@gmail.com>
2024-10-13 22:58:02 +0100

    Creation of the Book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,15 +1,18 @@
 class Book:
+    # Constructor to initialize a book instance
     def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
-        print(f"Book '{self.title}' by {self.author} has been created.")
 
+    # Destructor to handle book deletion
     def __del__(self):
-        print(f"Deleting '{self.title}'")
+        print(f"Deleting {self.title}")
 
+    # String representation for the user (readable form)
     def __str__(self):
-        return f"'{self.title}' by {self.author}, published in {self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
 
+    # Official representation of the object (for developers)
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
